b03dc51b673c3040ed6dbfbd2c042e65d435efc7
Enrique Madrid <emadridm@gmail.com>
2021-11-18 12:38:19 -0500

    TSX support added

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -11,8 +11,16 @@
 
 (straight-use-package 'smartparens)
 
+(require 'smartparens-config)
+
 (smartparens-global-mode 1)
 
+;; You may want to try smartparens-strict-mode.
+;; This enforces that pairs are always balanced,
+;; so commands like kill-line keep your code well-formed.
+;; (smartparens-strict-mode)
+
+;; Emacs lisp support
 (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
 
 (provide 'init-smartparens)
--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,3 +1,14 @@
+;;; init-web.el --- web-mode config -*- lexical-binding: t -*-
+
+;; Author: Enrique Madrid
+
+;;; Commentary:
+
+;; web-mode config for whole ecosystem.
+;; visit: https://web-mode.org/
+
+;;; Code:
+
 (straight-use-package 'web-mode)
 
 (use-package web-mode
@@ -9,6 +20,10 @@
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  :config
+  (setq web-mode-enable-auto-pairing nil))
 
 (provide 'init-web)
+
+;;; init-web.el ends here
