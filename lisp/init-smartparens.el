ba468f3ac524fe9bc9eb8b0cd229af2ef620fc11
Enrique Madrid <emadridm@gmail.com>
2021-11-17 19:17:32 -0500

    backup

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,5 +1,20 @@
+;;; init-smartparens.el --- smartparens settings -*- lexical-binding: t -*-
+
+;; Author: Enrique Madrid
+
+;;; Commentary:
+
+;; smartparens config for all ecosystem
+;; visit: https://github.com/Fuco1/smartparens
+
+;;; Code:
+
 (straight-use-package 'smartparens)
 
 (smartparens-global-mode 1)
 
+(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+
 (provide 'init-smartparens)
+
+;;; init-smartparens.el ends here
--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,32 +1,11 @@
-;;; init-yasnippet.el --- summary -*- lexical-binding: t -*-
+;;; init-yasnippet.el --- yasnippet config -*- lexical-binding: t -*-
 
 ;; Author: Enrique Madrid
-;; Maintainer: Enrique Madrid
-;; Version: version
-;; Package-Requires: (dependencies)
-;; Homepage: homepage
-;; Keywords: keywords
-
-
-;; This file is not part of GNU Emacs
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 
 ;;; Commentary:
 
-;; commentary
+;; yasnippet config for all ecosystem
+;; visit: https://github.com/joaotavora/yasnippet
 
 ;;; Code:
 
@@ -34,12 +13,9 @@
 (straight-use-package 'yasnippet-snippets)
 
 ;; (yas-global-mode 1)
-
 (require 'yasnippet)
 (yas-reload-all)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
-
-(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
 
 ;; (use-package yasnippet
 ;;   :config
