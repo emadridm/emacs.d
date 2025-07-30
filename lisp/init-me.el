d2bcfbd201e906d235641a6ee8c51ea0915983c0
Enrique Madrid <emadridm@gmail.com>
2025-07-30 17:53:36 -0500

    init-me.el refactored

--- lisp/init-me.el
+++ lisp/init-me.el
@@ -2,6 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
+;; Maximize the initial Emacs frame
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
 ;; Menu and Tool bar disable
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -9,10 +12,8 @@
 ;; Emacs's backup files disable
 (setq make-backup-files nil)
 
-;; Set font size equal to (display-pixel-height) * 12%
-;; (set-face-attribute 'default nil :height 200)
-;; (set-face-attribute 'default nil :height (round (* (display-pixel-height) 0.182)))
-(set-face-attribute 'default nil :height (round (* (nth 3 (frame-monitor-attribute 'geometry)) 0.14)))
+;; Set default FACE font height attribute
+(set-face-attribute 'default nil :height 160)
 
 ;; Truncate lines indicator disable.
 (setq-default truncate-lines t)
@@ -26,8 +27,6 @@
 ;; Mi globals keys
 (global-set-key (kbd "M-g F") 'find-file-at-point)
 (global-set-key (kbd "M-g U") 'browse-url-at-point)
-
-;; (setq projectile-create-missing-test-files t)
 
 (provide 'init-me)
 ;;; init-me.el ends here
